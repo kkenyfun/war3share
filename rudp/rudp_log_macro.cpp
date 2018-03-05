350bb2ead3bba55c4c75ba1f1a60de944b697e3f
fangqq <kenyfun@DESKTOP-9K7AA8Q>
2018-03-05 11:52:09 +0800

    all

--- rudp/rudp_log_macro.cpp
+++
@@ -1,9 +0,0 @@
-#include "rudp_log_macro.h"
-
-#ifndef DISABLE_RUDPLOG
-	#ifdef _DEBUG
-		SingleLogStream rudp_log("rudpdll", BaseLogStream::debug);
-	#else
-		SingleLogStream rudp_log("rudpdll", BaseLogStream::general);
-	#endif
-#endif
